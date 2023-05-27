331b9c38b207a33fb3ccd164d09b8887316576b4
hyejooLim <lhyejoo@naver.com>
2023-05-27 18:53:05 +0900

    add: Cart 페이지에 useGetCartItems Hook 추가

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,9 +4,11 @@ import AppLayout from '../components/layout/AppLayout';
 import CartItemList from '../components/cart/CartItemList';
 import CartSummary from '../components/cart/CartSummary';
 import { cartItemsState } from '../recoil/cart';
+import { useGetCartItems } from '../hooks/query/cartItems';
 import { CartHeader, CartBody } from '../styles/pages/Cart';
 
 const Cart = () => {
+  useGetCartItems();
   const cartItems = useRecoilValue(cartItemsState);
 
   return (
