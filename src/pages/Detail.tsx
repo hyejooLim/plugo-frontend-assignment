2a4d99b03a0a404caa675bdd45397574f2563637
hyejooLim <lhyejoo@naver.com>
2023-05-27 20:39:51 +0900

    add: Detail 페이지에 useGetProduct Hook 추가

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Product } from '../types';
 import AppLayout from '../components/layout/AppLayout';
 import ProductDetail from '../components/product/ProductDetail';
-
-const dummyData = {
-  id: 1,
-  name: 'top1',
-  category: {
-    id: 1,
-    name: 'top',
-    products: [],
-    productsCount: 10,
-  },
-  categoryId: 1,
-  color: { id: 4, name: 'YELLOW', products: [], productsCount: 6 },
-  colorId: 4,
-  imageUrl: 'https://img.pikbest.com/png-images/qianku/children-s-day-clothes_2240459.png!f305cw',
-  price: 100000,
-};
+import { useGetProduct } from '../hooks/query/product';
 
 const Detail = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState<Product>(dummyData);
+  const { data: product, isLoading } = useGetProduct(Number(id));
 
   return (
     <AppLayout>
-      <ProductDetail product={product} />
+      <ProductDetail product={product} isLoading={isLoading} />
     </AppLayout>
   );
 };
