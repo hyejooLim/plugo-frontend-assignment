e477524844d411cb9f022b725240c73de377a72a
hyejooLim <lhyejoo@naver.com>
2023-05-27 13:49:48 +0900

    feat: 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-  return <div>App</div>;
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+import NewArrivals from './pages/NewArrivals';
+import Category from './pages/Category';
+import Detail from './pages/Detail';
+import Cart from './pages/Cart';
+import Manage from './pages/Manage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/new-arrivals' element={<NewArrivals />} />
+        <Route path='/category/:id' element={<Category />} />
+        <Route path='/product/:id' element={<Detail />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/manage' element={<Manage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
