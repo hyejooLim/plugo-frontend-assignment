39f94a051b2466a74dfa081a3690d0c58b1806aa
hyejooLim <lhyejoo@naver.com>
2023-05-27 19:56:08 +0900

    design: Manage 페이지 UI 구현

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -1,5 +1,18 @@
+import AppLayout from '../components/layout/AppLayout';
+import { ManageWrapper } from '../styles/pages/Manage';
+
 const Manage = () => {
-  return <div>Manage</div>;
+  return (
+    <AppLayout>
+      <ManageWrapper>
+        <div className='center_area'>
+          <img src='https://shop-static.plugo.world/7b9a07bcdde434b0237ef65f338664e8.svg' />
+          <button className='login_button'>LOGIN</button>
+        </div>
+        <button className='create_product_button'>Create Product</button>
+      </ManageWrapper>
+    </AppLayout>
+  );
 };
 
 export default Manage;
---
+++ src/styles/pages/Manage.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const ManageWrapper = styled.div`
+  width: 620px;
+  height: 300px;
+  margin: 0 auto;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .center_area {
+    display: flex;
+    align-items: center;
+  }
+
+  .login_button {
+    width: 100%;
+    height: 50px;
+    margin-left: 20px;
+    font-size: 18px;
+    color: #fff;
+    background-color: #62b9a9;
+  }
+
+  .create_product_button {
+    margin-top: 80px;
+    padding: 20px 60px;
+    font-size: 22px;
+    background-color: #fff;
+    border: 1px solid #c6c6c6;
+    border-radius: 6px;
+
+    &:hover {
+      border: 1px solid #666;
+    }
+  }
+`;
